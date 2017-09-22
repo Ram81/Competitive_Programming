d679edcbbeb8a9e6fcbfe87a935d643b2314c0cf
Ram81 <ramramrakhya81@gmail.com>
2017-09-22 22:43:12 +0530

    Updated Segment Tree Template

--- Segment Tree/segtree.cpp
+++ Segment Tree/segtree.cpp
@@ -1,5 +1,27 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+
+#define mp make_pair
+#define pb push_back
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> pp;
+typedef pair<pp,pp> ppi;
+typedef vector<int> vv;
+
+const int MOD = (int) 1e9 + 7;
+const int INF = (int) 1e9;
+const ll LINF = (ll) 1e18;
+const ld PI = acos((ld) -1);
+const ld EPS = 1e-9;
+ll gcd(ll a, ll b) {ll r; while (b) {r = a % b; a = b; b = r;} return a;}
+ll lcm(ll a, ll b) {return a / gcd(a, b) * b;}
+ll fpow(ll n, ll k, int p = MOD) {ll r = 1; for (; k; k >>= 1) {if (k & 1) r = r * n % p; n = n * n % p;} return r;}
+void addmod(int& a, int val, int p = MOD) {if ((a = (a + val)) >= p) a -= p;}
+void submod(int& a, int val, int p = MOD) {if ((a = (a - val)) < 0) a += p;}
+int mult(int a, int b, int p = MOD) {return (ll) a * b % p;}
+int inv(int a, int p = MOD) {return fpow(a, p - 2, p);}
 
 ll st[400005];
 ll a[100005];
@@ -73,6 +95,11 @@ void lazyUpdate(int s,int e,int l,int r,int i,int val)
 	st[i]=st[2*i+1]+st[2*i+2];
 }
 
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	
+	return 0;
+}
 
 
 
