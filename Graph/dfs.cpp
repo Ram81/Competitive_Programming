8fd2a81501b969f4fc695281c49f6bf6cb0215d6
Ram81 <ramramrakhya81@gmail.com>
2017-09-22 22:41:47 +0530

    Updated DFS Template

--- Graph/dfs.cpp
+++ Graph/dfs.cpp
@@ -1,34 +1,44 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-vector<int> adj[100005];
-int v[100005]={0};
+#define mp make_pair
+#define pb push_back
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> pp;
+typedef pair<pp,pp> ppi;
+typedef vector<int> vv;
+
+const int MOD = (int) 1e9 + 7;
+const int INF = (int) 1e9;
+const ll LINF = (ll) 1e18;
+const ld PI = acos((ld) -1);
+const ld EPS = 1e-9;
+ll gcd(ll a, ll b) {ll r; while (b) {r = a % b; a = b; b = r;} return a;}
+ll lcm(ll a, ll b) {return a / gcd(a, b) * b;}
+ll fpow(ll n, ll k, int p = MOD) {ll r = 1; for (; k; k >>= 1) {if (k & 1) r = r * n % p; n = n * n % p;} return r;}
+void addmod(int& a, int val, int p = MOD) {if ((a = (a + val)) >= p) a -= p;}
+void submod(int& a, int val, int p = MOD) {if ((a = (a - val)) < 0) a += p;}
+int mult(int a, int b, int p = MOD) {return (ll) a * b % p;}
+int inv(int a, int p = MOD) {return fpow(a, p - 2, p);}
+
+vv v[100005];
+int vis[100005]={0};
 
 void dfs(int u)
 {
-	if(v[u])
+	if(vis[u])
 		return;
-	for(vector<int>::iterator it=v[u].begin();it!=v[u].end();it++)
-		if(!v[*it])
+	for(vv::iterator it=v[u].begin();it!=v[u].end();it++)
+		if(!vis[*it])
 			dfs(*it);
 	
 }
 
-void bfs(int u)
-{
-	queue<int> q;
-	q.push(u);
-	while(!q.empty())
-	{
-		u=q.front();
-		q.pop();
-		for(vector<int>::iterator it=v[u].begin();it!=v[u].end();it++)
-			if(!v[*it])
-	}
-}
-
-int main()
-{
-
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	
 	return 0;
 }
+
