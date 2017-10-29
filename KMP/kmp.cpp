7b14ca57b93a84d125257ee70b6732a18b894c3f
Ram81 <ramramrakhya81@gmail.com>
2017-10-29 21:55:01 +0530

    Update in KMP

--- KMP/kmp.cpp
+++ KMP/kmp.cpp
@@ -75,6 +75,17 @@ int KMP()
 	return ans;
 }
 
+bool chk(string str1, string str2, int m, int n)
+{
+   int j = 0; 
+   for (int i=0; i<n&&j<m; i++)
+       if (str1[j] == str2[i])
+         j++;
+ 
+   return (j==m);
+}
+ 
+ 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);	
